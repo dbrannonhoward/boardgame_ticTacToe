97512e122d918e932177a3898ae8a714295af4b2
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-10 00:46:42 -0500

    more pythonic function names

--- interactions.py
+++ interactions.py
@@ -3,19 +3,19 @@ from data_validation import argument_is_digit
 from human_stuff import Player
 
 
-def ask_number_of_players():
+def get_number_of_players():
     try:
         number_of_players = input("how many people are playing? [1] or [2]\n")
         if argument_is_digit(number_of_players) and 0 < int(number_of_players) < 3:
             return int(number_of_players)
         else:
             print("sorry that is not a valid response, try again")
-            return int(ask_number_of_players())
+            return int(get_number_of_players())
     except IOError as player_input_error:
         raise player_input_error
 
 
-def greet_user_and_set_name():
+def get_first_player_name():
     try:
         name_ = input("hey player, what is your name_?\n")
         if argument_is_alpha(name_):
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from game_stuff import Game
-from interactions import ask_number_of_players
-from interactions import greet_user_and_set_name
+from interactions import get_number_of_players
+from interactions import get_first_player_name
 from logging_stuff import Logger
 
 
 def main():
     game_log = Logger("ticTacToeLogger")
     game_log.info("requesting username")
-    player_one = greet_user_and_set_name()
-    number_of_players = ask_number_of_players()
+    player_one = get_first_player_name()
+    number_of_players = get_number_of_players()
     game_board = Game(number_of_players, player_one)
 
 
